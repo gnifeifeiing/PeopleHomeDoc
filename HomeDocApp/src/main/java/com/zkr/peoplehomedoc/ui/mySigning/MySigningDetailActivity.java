9403e77a9cf5430f26446c25d2a7fa1a2b9be2e6
gnifeifeiing <645485165@qq.com>
2016-10-09 18:50:36 +0800

    home修改

--- HomeDocApp/src/main/java/com/zkr/peoplehomedoc/ui/mySigning/MySigningDetailActivity.java
+++ HomeDocApp/src/main/java/com/zkr/peoplehomedoc/ui/mySigning/MySigningDetailActivity.java
@@ -1,20 +1,45 @@
 package com.zkr.peoplehomedoc.ui.mySigning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.zkr.peoplehomedoc.R;
 import com.zkr.peoplehomedoc.base.BaseActivity;
+import com.zkr.peoplehomedoc.widget.TitleBarUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
- * @Description: -------我的签约详情
  * @author --------------LF
+ * @Description: -------我的签约详情
  * @date ----------------2016/10/9 17:19
  */
 public class MySigningDetailActivity extends BaseActivity {
+
+    @Bind(R.id.titleBar)
+    TitleBarUtils titleBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_signing_detail);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    private void init() {
+        initTitle();
+    }
+
+    private void initTitle() {
+        titleBar.setTitle("签约明细");
+        titleBar.setLeftButtonClick(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
